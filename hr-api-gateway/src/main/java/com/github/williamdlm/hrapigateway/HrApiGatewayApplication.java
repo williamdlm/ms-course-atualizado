5488c103fea14658df820f50175d792ab135c93b
William Mota <william.mota.ext@spms.min-saude.pt>
2024-04-23 14:05:23 +0100

    Configuring api gateway

--- hr-api-gateway/src/main/java/com/github/williamdlm/hrapigateway/HrApiGatewayApplication.java
+++ hr-api-gateway/src/main/java/com/github/williamdlm/hrapigateway/HrApiGatewayApplication.java
@@ -1,13 +1,15 @@
 package com.github.williamdlm.hrapigateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class HrApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HrApiGatewayApplication.class, args);
+    }
 
 }
